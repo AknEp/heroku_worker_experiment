b6939b0b01c5494f559455a51767287500e4a5a3
Satoshi SUZUKI <akn@s-satoshi.net>
2018-02-27 15:03:49 +0900

    fix redis

--- config/initializers/resque.rb
+++ config/initializers/resque.rb
@@ -1,7 +1,7 @@
 if defined?(Resque)
-  begin
-    Resque.redis = ENV['REDIS_URL'] || 'localhost:6379'
-  rescue => e
-    puts e
+  if ENV['REDIS_URL']
+    Resque.redis = Redis.new(:url => ENV['REDIS_URL'])
+  else
+    Resque.redis = Redis.new(:url => 'localhost:6379')
   end
 end
